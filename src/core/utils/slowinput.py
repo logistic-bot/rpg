a24a79b409de6dfb10858916afc4346f7dc804b0
Khais COLIN <kh.col@orange.fr>
2020-05-20 19:05:32 +0200

    Update slowinput to include char_count_interval and by_word

--- src/core/utils/slowinput.py
+++ src/core/utils/slowinput.py
@@ -5,14 +5,16 @@ This file implements the slowinput function.
 from src.core.utils.slowprint import slowprint
 
 
-def slowinput(text: str,
+def slowinput(text: str,  # pylint: disable=R0913
               interval: float = 0.03,
               end: str = "",
               end_interval: float = 0,
-              fast: bool = False) -> str:
+              fast: bool = False,
+              char_count_interval: int = 0,
+              by_word: bool = False) -> str:
     """
     This functions call slowprint with all provided arguments, then calls input() and return the
     result. For all parameters, see slowprint()
     """
-    slowprint(text, interval, end, end_interval, fast)
+    slowprint(text, interval, end, end_interval, fast, char_count_interval, by_word)
     return input()
