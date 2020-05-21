3b95bee8399e46ec61aa1d292d6c71b79791942d
Khais COLIN <kh.col@orange.fr>
2020-05-21 18:34:19 +0200

    Config can be saved

--- main.py
+++ main.py
@@ -7,20 +7,21 @@ Main.
 from src.core.utils.slowinput import slowinput
 from src.core.utils.slowprint import slowprint
 
-import src.config as config
+from src.config import config, save
+assert isinstance(config["fastprint"], bool)
 
-slowprint("Please choose a name for your character.", end_interval=0, fast=config.fastprint,
+slowprint("Please choose a name for your character.", end_interval=0, fast=config["fastprint"],
           by_word=True, interval=0.03)
-slowprint('It will be used like this: "Hello `<name>`, how are you?"', fast=config.fastprint,
+slowprint('It will be used like this: "Hello `<name>`, how are you?"', fast=config["fastprint"],
           end_interval=0, by_word=True, interval=0.03)
-slowprint("You can use your real name, or invent one!", fast=config.fastprint, by_word=True,
+slowprint("You can use your real name, or invent one!", fast=config["fastprint"], by_word=True,
           interval=0.03, end_interval=0)
-NAME = slowinput("name> ", fast=config.fastprint)
-print()
+config["user"]["name"] = slowinput("name> ", fast=config["fastprint"])
+save()
 
 print()
 print()
-slowprint("----------------------------------------", fast=config.fastprint, interval=0.01,
+slowprint("----------------------------------------", fast=config["fastprint"], interval=0.01,
           end_interval=0)
 print()
 print()
--- src/config.py
+++ src/config.py
@@ -1,7 +1,27 @@
 # pylint: disable=C0103
 
-"""
-This file contains configurations options for the whole game. Change with caution.
-"""
+"""This file loads configurations options for the whole game. Change the path below to load a
+custom config. """
 
-fastprint = False
+import json
+from pathlib import Path
+
+CONFIG_PATH = Path("~/.config/rpg.json").expanduser().resolve()
+
+
+def _load_config(path) -> dict:
+    with open(path, "r") as file:
+        return json.load(file)
+
+
+def _save_config(config_object, path) -> None:
+    with open(path, "w") as file:
+        json.dump(config_object, file)
+
+
+def save():
+    _save_config(config, CONFIG_PATH)
+
+
+config = {}
+config.update(_load_config(CONFIG_PATH))
